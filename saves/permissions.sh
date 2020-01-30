03dfbe464ddd8e38f015752c141328836c7abb0f
hugo <hugo.bilquart@gmail.com>
2020-01-30 17:15:00 +0100

    Reupload project

--- saves/permissions.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sudo chown www-data:www-data /var/www/html
-sudo chmod -R 775 /var/www/html/projet
-sudo chmod -R 777 /var/www/html/projet/db
-sudo chmod -R 777 /var/www/html/projet/images
